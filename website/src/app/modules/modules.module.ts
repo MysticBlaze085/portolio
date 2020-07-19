09eb65d2c31ff89212d9601d8a628bad943b148a
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-07-19 12:22:14 -0500

    added skillset

--- website/src/app/modules/modules.module.ts
+++ website/src/app/modules/modules.module.ts
@@ -4,9 +4,10 @@ import { SHARED_MODULES } from './shared';
 import { MODULES_COMPONENTS, MODULES_CONTAINERS } from '.';
 import { ShowcaseComponent } from './showcase/showcase.component';
 import { AboutComponent } from './about/about.component';
+import { SkillsetComponent } from './skillset/skillset.component';
 
 @NgModule({
-    declarations: [...MODULES_COMPONENTS, ...MODULES_CONTAINERS, ShowcaseComponent, AboutComponent],
+    declarations: [...MODULES_COMPONENTS, ...MODULES_CONTAINERS, ShowcaseComponent, AboutComponent, SkillsetComponent],
     imports: [CommonModule, ...SHARED_MODULES],
     exports: [...MODULES_COMPONENTS, ...MODULES_CONTAINERS],
 })
