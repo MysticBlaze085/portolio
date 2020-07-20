5741cc00e1605f3765af90ec7cd632c3f2697052
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-07-20 14:55:22 -0500

    stoped on ngx-lightbox install

--- website/src/app/modules/portfolio/containers/projects/projects.component.ts
+++ website/src/app/modules/portfolio/containers/projects/projects.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ISectionTitleConfig } from 'src/app/interfaces/section-title.interface';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+    selector: 'app-projects',
+    templateUrl: './projects.component.html',
+    styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+    sectionTitleConfig: ISectionTitleConfig = { title: 'Portfolio', class: 'text-dark' };
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
