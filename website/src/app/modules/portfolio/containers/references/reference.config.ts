1c98e7fb2e95adea525881dfe4a4160de9aef458
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-07-22 13:15:19 -0500

    updated references

--- website/src/app/modules/portfolio/containers/references/reference.config.ts
+++ website/src/app/modules/portfolio/containers/references/reference.config.ts
@@ -8,6 +8,13 @@ export const references = [
         linkText: 'Verdant Services',
     },
     {
+        image: 'https://placeimg.com/40/40/animals',
+        description: 'Your skill level with Angular is very competitive.',
+        author: 'Michael Kelly',
+        link: 'http://verdantservices.com',
+        linkText: 'Verdant Services',
+    },
+    {
         image: 'https://placeimg.com/40/40/arch',
         description:
             'Melissa did an excellent website design for my business that has customers emailing me every day! I used to get an inquiry email once in a week to ten days. I now receive one or two each day! Wow! I have an extremely talented daughter that puts her heart in to her projects! Thanks Melissa!',
